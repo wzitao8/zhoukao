de21e95b5e8c583beef5a2e0c0b1e82fc3cae174
admin <index@localhost.localdomain>
2019-04-16 18:45:54 +0800

    wwww

--- app/Http/Controllers/WxController.php
+++ app/Http/Controllers/WxController.php
@@ -16,14 +16,39 @@ class WxController extends Controller
         $content =file_get_contents("php://input");
         $time = date('Y-m-d H:i:s');
         $str =$time .$content ."\n";
+
+        $data = simplexml_load_string($content);
+        $wx_id = $data->ToUserName;
+        $openid = $data ->FromUserName;
         file_put_contents("logs/wx_event.log",$str,FILE_APPEND);
-        echo 'SUCCESS';
+        $wx_id = $data->ToUserNamel;    //公众号id
+        $openid = $data->FromUserName;  //用户openid
+        $event = $data->Event;      //
+        if($event=='subscribe'){
+            $local_user = WxUserModel::where(['openid'])->first();
+            if($local_user){
+                echo '<xml><ToUserName><![CDATA['.$openid.']></ToUserName><FromUserName><![CDATA['.$wx_id.']></FromUserName></xml>';
+            }else{
+                $u = $this->getUserInfo($openid);
+                echo '<xml><ToUserName><![CDATA['.$openid.']></ToUserName><FromUserName><![CDATA['.$wx_id.']]></FromUserName></xml>';
+
+                //用户信息入库
+                $u_id = [
+                    'openid'=>$u['openid'],
+                    'nickname'=> $u['nickname'],
+                    'sex' => $u['sex'],
+                    'headimgurl'=>$u['headimgurl'],
+                ];
+            }
+        }
+
     }
-    
+
     public function getAccessToken(){
         //是否有缓存
         $key ='wx_accsee_token';
         $token = Redis::get($key);
+
 //        var_dump($token);
         if($token){
             echo 'Cache';
@@ -43,5 +68,20 @@ class WxController extends Controller
     public function test(){
         $access_token = $this->getAccessToken();
         echo 'token:'.$access_token;echo'</br>';
+    }
+
+
+    public function sendMsg($openid_arr,$content){
+        $msg = [
+            "touser" => $openid_arr,
+            "msgtype" => "text",
+            "text" => [
+                "content" => $content
+            ]
+        ];
+        $data = json_encode($msg,JSON_UNESCAPED_UNICODE);
+
+        $url = '';
+
     }
 }
